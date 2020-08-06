bb46b0a8614d2604b1382e83c3e1041a81785f7b
mmaddex <matt.maddex@auth0.com>
2020-08-06 13:49:17 -0700

    remove unneeded config

--- server/index.js
+++ server/index.js
@@ -26,13 +26,13 @@ export default function (cfg, storageProvider) {
   // dashboardAdmins route allows login with dashboard admin credentials
   app.use(routes.dashboardAdmins({
     secret: config('EXTENSION_SECRET'),
-    audience: 'urn:breaking-changes',
+    audience: 'urn:example-extension',
     rta: config('AUTH0_RTA').replace('https://', ''),
     domain: config('AUTH0_DOMAIN'),
     baseUrl: config('PUBLIC_WT_URL'),
     webtaskUrl: config('PUBLIC_WT_URL'),
-    clientName: 'Breaking Changes',
-    sessionStorageKey: 'breaking-changes:apiToken',
+    clientName: 'Example Extension',
+    sessionStorageKey: 'example-extension:apiToken',
     // whether or not request access token. if access token is requested, it'll be used to access management api
     // otherwise the client credentials will be used
     noAccessToken: true,
